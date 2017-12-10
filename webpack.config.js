16f01c31a2e4018e6bdb088a1fd2e6f57ffc0759
Kyle Lawlor <klawlor419@gmail.com>
2017-12-10 00:05:11 +0000

    fix filepath

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './src/app.js',
   output: {
-    filename: 'dist/bundle.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   }
 }
