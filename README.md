52511ddc389fe9da2e50b762937eaccc5f66fe62
Kyle Lawlor <klawlor419@gmail.com>
2017-12-10 22:41:05 +0000

    clean up

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-yarn run build
-see package.json
+Install yarn, then:
+
+    yarn run build --config <webpack.config>
+
+Outputs `./dist/bundle.js`.
+Note that this setup requires configuration in webpack for new js src files.
+See `package.json` for more.
+
+
+Install `pipenv`, then:
+
+    # from project directory
+    pipenv install
+
+
+Flask app config precedence:
+
+1. system env variables
+2. `instance/.env`
+3. `backend/settings.py`
+
+
+Improvements: 
+
+- http://flask.pocoo.org/docs/dev/config/#development-production
--- backend/__init__.py
+++ backend/__init__.py
@@ -1,8 +1,17 @@
+import os
+import logging
 from flask import Flask
+from dotenv import load_dotenv
+
 
 def create_app():
     app = Flask(__name__)
-    @app.route('/')
-    def index():
-       return 'helloworld'
+
+    # load from .env if present, otherwise fallback on dev values
+    # system environment variables take precedence.
+    # see https://github.com/theskumar/python-dotenv#usages for more.
+    dotenv_path = os.path.join(app.instance_path, '.env')
+    load_dotenv(dotenv_path)
+    app.config.from_object('backend.settings')
+
     return app
