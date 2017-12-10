cf13c66f8a49744b6a3bab8e101acee68a473a00
Kyle Lawlor <klawlor419@gmail.com>
2017-12-10 00:58:58 +0000

    begin refactor to common

--- webpack.common.js
+++ webpack.common.js
@@ -1,9 +1,11 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/app.js',
+  entry: {
+    app: './src/app.js'
+  },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   }
 }
